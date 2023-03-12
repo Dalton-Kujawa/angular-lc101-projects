0db0abb8b1d9ed711ceb415325210df68930cd59
Dalton-Kujawa <daltonkujawa@yahoo.com>
2023-03-12 17:42:48 -0500

    Final Changes

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  photosTitle = 'Random Photos';
+  image1 = 'https://i.ytimg.com/vi/tPaJ8lSFap8/maxresdefault.jpg';
+  image2 = 'https://s3-media0.fl.yelpcdn.com/bphoto/mFYmHO2AhPP7wbn_gLzqtg/348s.jpg';
+  image3 = 'https://media.wired.com/photos/5932619426780e6c04d2b4c4/191:100/w_1280,c_limit/awkward-seal-ft.jpg';
 
   constructor() { }
 
